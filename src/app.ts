29e903abd657512a34cbdf84191866fada2a2dac
steveunixson <unixson@gmail.com>
2019-06-26 15:37:44 +0300

    first working prototype

--- src/app.ts
+++ src/app.ts
@@ -5,29 +5,6 @@ import WebSocket from 'ws';
 import JSONResponce from './helpers/JSONResponse.class';
 import log from './helpers/WinstonLogger.class';
 
-const wsSettings = {
-  port: 9020,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      // See zlib defaults.
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3,
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024,
-    },
-    // Other options settable:
-    clientNoContextTakeover: true, // Defaults to negotiated value.
-    serverNoContextTakeover: true, // Defaults to negotiated value.
-    serverMaxWindowBits: 10, // Defaults to negotiated value.
-    // Below options specified as default values.
-    concurrencyLimit: 10, // Limits zlib concurrency for perf.
-    threshold: 1024, // Size (in bytes) below which messages
-    // should not be compressed.
-  },
-};
-
 class App {
   public app: Express;
 
@@ -41,27 +18,39 @@ class App {
     this.port = port;
     this.app = express();
     this.app.use(bodyParser.json({ limit: '50mb' }));
-    this.app.get('/', (req, res): void => {
-      JSONResponce.success(req, res, [], 'hello world!');
-    });
     this.server = http.createServer(this.app);
-    this.wss = new WebSocket.Server(wsSettings);
+    this.wss = new WebSocket.Server({ server: this.server, port: 9020 });
   }
 
-  public async start(): Promise<void> {
-    await this.app.listen(this.port);
-    this.wss.on('connection', (ws: WebSocket): void => {
+  private async routes(): Promise<void> {
+    await this.app.get('/', (req, res): void => {
+      JSONResponce.success(req, res, [], 'hello world!');
+    });
+  }
+
+  private async events(): Promise<void> {
+    await this.wss.on('connection', (ws: WebSocket): void => {
       ws.on('message', (message: string): void => {
         log.info(`received: ${message}`);
         ws.send(`Hello, you sent -> ${message}`);
       });
       ws.send('Hi there, I am an Octopus WebSocket server');
+      this.app.get('/test', (req, res): void => {
+        ws.send('test');
+        JSONResponce.success(req, res, [{ clients: this.wss.clients }], 'Test started!');
+      });
     });
+  }
+
+  public async start(): Promise<void> {
+    await this.app.listen(this.port);
+    await this.routes();
+    await this.events();
   }
 }
 
 
-const app = new App(3001);
+const app = new App(3000);
 app
   .start()
   .then((): void => {
